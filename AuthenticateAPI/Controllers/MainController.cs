fdf732653e580cfc3a803f87ba95b9cefafa33f3
Satya <satyaa.prakash@outlook.com>
2020-05-08 20:42:14 -0400

    * .gitignore: Initial commit for Token Code Generation.
    * Identity.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * AuthenticateAPI.csproj:
    * appsettings.Development.json:
    * MainController.cs:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ AuthenticateAPI/Controllers/MainController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuthenticateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MainController : ControllerBase
+    {
+        [HttpGet("gettoken") ]
+        public object GetToken()
+        {
+            string key = "my-secret-12345";
+            var issuer = "http://satya.com";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var permClaims = new List<Claim>();
+            permClaims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            permClaims.Add(new Claim("Valid", "1"));
+            permClaims.Add(new Claim("userid", "1"));
+            permClaims.Add(new Claim("name", "bilal"));
+
+            var token = new JwtSecurityToken(issuer, issuer, permClaims, expires:DateTime.Now.AddDays(1), signingCredentials: credentials);
+            var jwt_token = new JwtSecurityTokenHandler().WriteToken(token);
+            return new { data = jwt_token };
+        }
+    }
+}
